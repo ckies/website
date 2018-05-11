b0ef7a74777b137c735087263d99992d13a28c68
sbstjn <mail@sbstjn.com>
2018-05-11 23:22:49 +0200

    Update Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,14 +4,12 @@ var gulpShell = require('gulp-shell')
 var gulpTypeScript = require('gulp-typescript')
 var gulpSourcemaps = require('gulp-sourcemaps')
 
-gulp.task('build:styles',
+gulp.task('styles:build',
   () => gulp.src('source/styles/main.sass').pipe(
     gulpSourcemaps.init()
   ).pipe(
     gulpSass(
-      {
-        outputStyle: 'compressed'
-      }
+      { outputStyle: 'compressed' }
     ).on('error', gulpSass.logError)
   ).pipe(
     gulpSourcemaps.write('.')
@@ -20,15 +18,16 @@ gulp.task('build:styles',
   )
 )
 
-gulp.task('build:scripts',
+gulp.task('styles:watch',
+  () => gulp.watch(['./source/styles/**/*.sass'], ['styles:build'])
+)
+
+gulp.task('scripts:build',
   () => gulp.src('source/scripts/*.ts').pipe(
     gulpSourcemaps.init()
   ).pipe(
     gulpTypeScript(
-      {
-        noImplicitAny: true,
-        out: 'main.js'
-      }
+      { noImplicitAny: true, out: 'main.js' }
     )
   ).js.pipe(
     gulpSourcemaps.write()
@@ -37,25 +36,42 @@ gulp.task('build:scripts',
   )
 )
 
-gulp.task('build:cookies',
+gulp.task('scripts:watch',
+  () => gulp.watch(['./source/scripts/*.ts'], ['scripts:build'])
+)
+
+gulp.task('cookies:build',
   gulpShell.task('ckies --silent --flat --language en --output ./content/cookies --format markdown')
 )
 
-gulp.task('build:hugo',
+gulp.task('cookies:watch',
+  () => gulp.watch(['./cookies.yml'], ['cookies:build'])
+)
+
+gulp.task('hugo:build',
   gulpShell.task('hugo --destination dist --quiet')
+)
+
+gulp.task('hugo:serve',
+  gulpShell.task('hugo serve')
+)
+
+gulp.task('serve',
+  [
+    'styles:build',
+    'scripts:build',
+    'cookies:build',
+    'hugo:serve',
+    'styles:watch',
+    'scripts:watch',
+    'cookies:watch'
+  ]
 )
 
 gulp.task('build',
   [
-    'build:styles',
-    'build:scripts',
-    'build:cookies',
-    'build:hugo'
+    'styles:build'
   ]
 )
 
-gulp.task('default',
-  [
-    'build'
-  ]
-)
+gulp.task('default', ['build'])
