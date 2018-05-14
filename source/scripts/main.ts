058957b6ce353192556c55939e15cda5695b653a
sbstjn <mail@sbstjn.com>
2018-05-14 13:42:19 +0200

    Add JavaScript to toggle settings

--- source/scripts/main.ts
+++ source/scripts/main.ts
@@ -1,7 +1,29 @@
-import { ckies } from '@ckies/library'
+import { ckies, CookieType } from '@ckies/library'
 
-console.log('Loaded 2')
+const handleEvent = (event: Event) => {
+  if (event.target instanceof HTMLInputElement) {
+    const type = event.target.getAttribute('data-cookie-type') as CookieType
 
-console.log(ckies.useFunctional(),ckies.useMarketing(),ckies.useNecessary(),ckies.useMarketing())
+    if (event.target.checked as Boolean) {
+      ckies.allow(type)
+    } else {
+      ckies.deny(type)
+    }
+  }
+}
 
-console.log('22')
+const initCheckbox = (item: Element) => {
+  const type = item.getAttribute('data-cookie-type') as CookieType
+
+  if (ckies.use(type)) {
+    item.setAttribute('checked', 'checked')
+  } else {
+    item.removeAttribute('checked')
+  }
+
+  item.addEventListener('change', handleEvent)
+}
+
+document.addEventListener('DOMContentLoaded', 
+  event => document.querySelectorAll(`input[data-cookie-type]`).forEach(initCheckbox)
+)
